d7cbccc74d42fb72de599ee29dacb7a2edf1c694
Moyush Borah <moyushborah@gmail.com>
2024-12-19 20:59:14 +0300

    Mobile logo changed

--- components/logo.tsx
+++ components/logo.tsx
@@ -17,7 +17,7 @@ export function LogoMobile() {
     <a href="/" className="flex items-center gap-2">
       
       <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
-        widowWeb
+        visualAise
       </p>
     </a>
   );
