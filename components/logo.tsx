a79995df9cf24da269862a374f22e99deb1ca953
Manav <manavbhatt056@gmail.com>
2024-12-19 23:35:53 +0530

    logo changes

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,8 +4,8 @@ import React from "react";
 function Logo() {
   return (
     <a href="/" className="flex items-center gap-2">
-      <View className="stroke h-11 w-11 stroke-red-500 stroke-[1.5]" />
-      <p className="bg-gradient-to-r from-red-800 to-black bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+      <View className="stroke h-11 w-11 stroke-blue-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
         visualAise
       </p>
     </a>
@@ -16,7 +16,7 @@ export function LogoMobile() {
   return (
     <a href="/" className="flex items-center gap-2">
       
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+      <p className="bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
         visualAise
       </p>
     </a>
